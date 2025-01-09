2b7f96cbf26b65584470c663f0c8f907da527d73
Matvey Vidyasov <kicksukoh@gmail.com>
2025-01-10 02:35:34 +0300

    feat: добавлен компонент Checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import './styles/global.css'
+import './styles/global.css';
 import Button from './components/Common/Button';
+import Checkbox from './components/Common/Checkbox';
 
 function App() {
-  return(
-    <>
-      <Button variant='primary'>1</Button>
-      <Button variant='secondary'>2</Button>
-      <Button variant='tertiary'>3</Button>
-    </>
-  );
+    return (
+        <>
+            <Button variant="primary">1</Button>
+            <Button variant="secondary">2</Button>
+            <Button variant="tertiary">3</Button>
+            <Checkbox label="Текст чекбокса" />
+            <Checkbox label="Текст чекбокса" disabled />
+        </>
+    );
 }
 
-export default App
+export default App;
